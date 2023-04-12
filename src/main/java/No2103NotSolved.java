86bd0b0662d2d5493d3f040f08ef1662cd050b20
UDADDY <gustmd5715@gmail.com>
2023-04-12 20:39:51 +0900

    2023/04/12

--- src/main/java/No2103NotSolved.java
+++ src/main/java/No2103NotSolved.java
@@ -1,48 +1,48 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-// 직교다각형
 public class No2103NotSolved {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        long[] arr = new long[N];
+        List<Point> list = new ArrayList<>();
 
-
-        for (int i = 0; i < N; i++)
-            arr[i] = sc.nextLong();
-        Arrays.sort(arr);
-
-        long denominator = arr[0];
-        for (int i = 1; i < N; i++) {
-            denominator = lcm(denominator, arr[i]);
-        }
-
-        int numerator = 0;
+        // Input
         for (int i = 0; i < N; i++) {
-            numerator += denominator / arr[i];
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            list.add(new Point(x, y));
         }
 
-        long gcd = gcd(denominator, numerator);
+        List<List<Point>> overlappedX = new ArrayList<>();
+        List<List<Point>> overlappedY = new ArrayList<>();
 
-        System.out.println(denominator / gcd + "/" + numerator / gcd);
-
-    }
-
-    public static long gcd(long a, long b) {
-        long c;
-        while (b != 0) {
-            c = a % b;
-            a = b;
-            b = c;
+        // Finding The Number Of Overlapping x, y
+        for (int i = 0; i < list.size(); i++) {
+            List<Point> tmp = new ArrayList<>();
+            overlappedX.add(tmp);
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) == list.get(j)) {
+                    tmp.add(list.get(j));
+                    list.remove(j);
+                }
+            }
         }
-        return a;
+
+
     }
 
-    public static long lcm(long a, long b) {
+}
 
-        return a * b / gcd(a, b);
+class Point {
+    public int x;
+    public int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-
 }
