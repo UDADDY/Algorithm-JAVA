3a3541653566e2cde79bd50787982eb75f1a263e
UDADDY <gustmd5715@gmail.com>
2023-04-14 22:52:23 +0900

    20230414

---
+++ src/main/java/DataStructure/LinkedListQueue.java
@@ -0,0 +1,63 @@
+package DataStructure;
+
+public class LinkedListQueue<E> {
+    LinkedListQueueNode<E> front;
+    LinkedListQueueNode<E> rear;
+    int size;
+
+    public LinkedListQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
+    public void enqueue(E item) {
+        LinkedListQueueNode<E> newNode = new LinkedListQueueNode<E>(item);
+
+        if (size == 0)
+            front = newNode;
+        else
+            rear.nextNode = newNode;
+        rear = newNode;
+        size++;
+    }
+
+    public E dequeue() {
+        if (size == 0)
+            return null;
+
+        LinkedListQueueNode<E> temp = front.nextNode;
+        E ret = front.item;
+        front.item = null;
+        front.nextNode = null;
+        front = temp;
+        size--;
+
+        return ret;
+    }
+
+//    public boolean empty() {
+//
+//    }
+//
+//    public int size() {
+//
+//    }
+
+}
+
+class LinkedListQueueNode<E> {
+    E item;
+    LinkedListQueueNode<E> nextNode;
+
+    public LinkedListQueueNode() {
+        nextNode = null;
+    }
+
+
+    public LinkedListQueueNode(E item) {
+        this.item = item;
+        nextNode = null;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+    }
+}
