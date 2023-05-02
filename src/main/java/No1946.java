c42229512dc773f3aabe29b7c6ba16c00f7c46e2
UDADDY <gustmd5715@gmail.com>
2023-05-02 12:45:41 +0900

    [BOJ] 1946 신입 사원

--- src/main/java/No1946.java
+++ src/main/java/No1946.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class No1946 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
