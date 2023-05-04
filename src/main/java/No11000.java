30cd494911a57e0f229f59327869c0117d1e2868
UDADDY <gustmd5715@gmail.com>
2023-05-04 13:54:05 +0900

    [BOJ] 11047 동전 0

--- src/main/java/No11000.java
+++ src/main/java/No11000.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class Main {
+public class No11000 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
