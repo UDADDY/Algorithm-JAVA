b5ed63bb228ecdca4f40bfea1c47027bcdd056da
유현승 <gustmd5715@gmail.com>
2026-06-13 14:44:58 +0900

    Time: 421 ms (95.92%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,67 +1,22 @@
 select distinct
     id,
-    TO_CHAR(visit_date, 'YYYY-MM-DD') as visit_date, 
-    people
+    TO_CHAR(VISIT_DATE, 'YYYY-MM-DD') AS VISIT_DATE,
+    NOW_ AS people
 from (
     select
-        *
-    from Stadium s
-    where 100 <= s.people
-        AND EXISTS (
-            select
-                1
-            from Stadium s2
-            where id = s.id + 1
-                AND 100 <= people
-        )
-        AND EXISTS (
-            select
-                1
-            from Stadium s3
-            where id = s.id + 2
-                AND 100 <= people
-        )
-
-    union all
-
-    select
-        *
-    from Stadium s
-    where 100 <= s.people
-        AND EXISTS (
-            select
-                1
-            from Stadium s2
-            where id = s.id - 1
-                AND 100 <= people
-        )
-        AND EXISTS (
-            select
-                1
-            from Stadium s3
-            where id = s.id + 1
-                AND 100 <= people
-        )
-
-    union all
-
-    select
-        *
-    from Stadium s
-    where 100 <= s.people
-        AND EXISTS (
-            select
-                1
-            from Stadium s2
-            where id = s.id - 2
-                AND 100 <= people
-        )
-        AND EXISTS (
-            select
-                1
-            from Stadium s3
-            where id = s.id - 1
-                AND 100 <= people
-        )
+        id,
+        visit_date,
+        LAG(people, 2) OVER(order by id asc) as Before_2,
+        LAG(people, 1) OVER(order by id asc) as Before_1,
+        people as Now_,
+        LEAD(people, 1) OVER(order by id asc) as After_1,
+        LEAD(people, 2) OVER(order by id asc) as After_2
+    from Stadium
 )
-order by id asc
+where 100 <= NOW_
+    AND (
+        (100 <= BEFORE_2 AND 100 <= BEFORE_1 AND 100 <= NOW_) -- 그제, 어제, 오늘
+        OR (100 <= BEFORE_1 AND 100 <= NOW_ AND 100 <= After_1) -- 어제, 오늘, 내일
+        OR (100 <= NOW_ AND 100 <= After_1 AND 100 <= After_2) -- 오늘, 내일, 모레
+    )
+order by id
