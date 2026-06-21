bccfb93de42e5612a961c42aad563046ba0c8dc4
유현승 <gustmd5715@gmail.com>
2026-06-21 18:42:46 +0900

    Time: 881 ms (66.66%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,28 +1,27 @@
-select
-    stu.student_id, 
-    stu.student_name, 
-    sub.subject_name,
-    -- case
-    --     when MAX(exam.student_id) is null then 0 
-    --     else count(*)
-    -- end as attended_exams
-    count(exam.student_id) as attended_exams
-from Students stu
-cross join Subjects sub
-left join Examinations exam on stu.student_id = exam.student_id
-    and sub.subject_name = exam.subject_name
-group by stu.student_id, stu.student_name, sub.subject_name
-order by stu.student_id
-
 -- select
---     -- stu.student_id, 
---     -- stu.student_name, 
---     -- sub.subject_name,
---     -- count(*) as attended_exams
---     *
+--     stu.student_id, 
+--     stu.student_name, 
+--     sub.subject_name,
+--     -- case
+--     --     when MAX(exam.student_id) is null then 0 
+--     --     else count(*)
+--     -- end as attended_exams
+--     count(exam.student_id) as attended_exams
 -- from Students stu
 -- cross join Subjects sub
 -- left join Examinations exam on stu.student_id = exam.student_id
 --     and sub.subject_name = exam.subject_name
--- -- group by stu.student_id, stu.student_name, sub.subject_name
+-- group by stu.student_id, stu.student_name, sub.subject_name
 -- order by stu.student_id
+
+select 
+    stu.student_id, 
+    stu.student_name, 
+    sub.subject_name ,
+    count(exam.student_id) as attended_exams
+from Students stu
+cross join Subjects sub
+left join Examinations exam
+    on stu.student_id=exam.student_id and sub.subject_name=exam.subject_name
+group by stu.student_id ,stu.student_name , sub.subject_name 
+order by stu.student_id ,sub.subject_name;
